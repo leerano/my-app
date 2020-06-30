6ec83bd242f692efeb20a0f7abcf76111d77b164
Ralph Serrano <leerano@yahoo.com>
2020-06-30 18:06:52 -0500

    update files

--- route/companies.js
+++ route/companies.js
@@ -1,2 +1,21 @@
 const express = require ('express');
-const 
+const router = express.Router();
+const connection = require('../connection');
+
+router.get('/', (req, res) => {
+    const sql = 'SELECT * FROM customers ORDER BY customerNumber LIMIT ?, ?';
+    const pageNumber = parseInt(req.params.pageNumber) || 1;
+    const pageSize = parseInt(req.params.pageSize) || 10;
+    const firstRecord = pasrseInt()
+
+    connection.query(sql, [firstRecord, pageSize], (error, results) =>{
+        if (error) {
+                console.log(error);
+
+        }
+        res.send(results);
+    });
+});
+
+
+module.exports = router;
